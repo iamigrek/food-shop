14d6b3de14cf21b2336ee297d9654745f0decc5c
iamigrek <gerevyanko.igorek@gmail.com>
2021-08-27 03:42:28 +0300

    Начал писать структуру для главной

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,18 @@
 import style from './Main.module.scss';
 
 function Main() {
-  return <div className={style.main}>Main</div>;
+  return (
+    <div className={style.main}>
+      <input type='text' placeholder='Search for food, coffe, etc..' />
+      <div className='main__filterBlock'>
+        <ul className={`listReset`}>
+          <li>
+            <button>Hot Dishes</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Main;
