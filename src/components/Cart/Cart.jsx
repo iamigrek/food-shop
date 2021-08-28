04ea334ca3469d3d2256c430f63ad9bddfe87cc8
iamigrek <gerevyanko.igorek@gmail.com>
2021-08-28 04:52:41 +0300

    Сверстал корзину

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,33 @@
 import style from './Cart.module.scss';
+import CartList from './CartList/CartList';
 
 function Cart() {
-  return <div className={style.cart}>Cart</div>;
+  return (
+    <div className={style.cart}>
+      <div>
+        <div className={style.cart__titles}>
+          <h4>Item</h4>
+          <div className={style.cart__titles_right}>
+            <h4>Qty</h4>
+            <h4>Price</h4>
+          </div>
+        </div>
+        <CartList />
+      </div>
+
+      <div className={style.cart__bottom}>
+        <div className={style.cart__total}>
+          <span className={style.cart__totalTitle}>Sub total</span>
+          <div className={style.cart__price}>
+            <span className={style.cart__priceCurrency}>$</span>
+            <span className={style.cart__priceValue}>3.99</span>
+          </div>
+        </div>
+
+        <button className={`btn btn__cart`}>Continue to Payment</button>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
